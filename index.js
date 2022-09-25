55ac698c49b876353f107a32eb4ed895fac899b9
Tom Va <tomtongva@gmail.com>
2022-09-25 17:41:29 -0400

    add some debug for db conn

--- index.js
+++ index.js
@@ -6,7 +6,24 @@ const jwtSecret = "Group3KeyForJWT";
 const headerTokenKey = "x-jwt-token";
 
 app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`)
+	for (const name of Object.keys(nets)) {
+        for (const net of nets[name]) {
+            // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+            // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
+            const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
+            if (net.family === familyV4Value && !net.internal) {
+                if (!results[name]) {
+                    results[name] = [];
+                }
+                results[name].push(net.address);
+            }
+        }
+    }
+    console.log(results);
+
+    //var os = require('os');
+    //console.log('Listening at http://' + os.hostname + ':${port}')
+    console.log(`Listening on port ${port}`);
 });
 
 const jwtValidateUserMiddleware = (req, res, next) => {
@@ -124,7 +141,9 @@ async function createUser(email, password, firstName, lastName, gender, city) {
 
 async function findUser(email, password) {
     try {
+		console.log("waiting to connect to db");
         await client.connect();
+		console.log("connected to db and looking for " + email + " " + password);
 
         var user;
         if (password != undefined)
