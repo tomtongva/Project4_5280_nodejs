bc0441b434ae0ef6077908e7bf32de9483b70619
Tom Va <tomtongva@gmail.com>
2022-09-25 20:51:40 -0400

    test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 8000;
+const port = 8080;
 const jwt  = require("jsonwebtoken");
 const jwtSecret = "Group3KeyForJWT";
 const headerTokenKey = "x-jwt-token";
